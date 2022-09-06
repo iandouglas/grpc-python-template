47c2bad71a77469c04381956a01cbdf88640aac9
Karl Behrens <karlbehrensg@gmail.com>
2022-09-05 22:35:11 -0400

    Create users table

---
+++ src/adapters/orm.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from config import get_db_url, get_settings
+
+settings = get_settings()
+
+SQLALCHEMY_DATABASE_URL = get_db_url()
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base(metadata=MetaData(schema=settings.db_schema))
+
+
+def get_db():
+    database = SessionLocal()
+    try:
+        yield database
+    finally:
+        database.close()
