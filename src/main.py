7136d4ace1841c1a6c64f585999bee387ae798e2
Karl Behrens <karlbehrensg@gmail.com>
2022-09-06 02:17:35 -0400

    Add server reflection

--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 import grpc
+from grpc_reflection.v1alpha import reflection
 from concurrent import futures
 
 import definition_pb2 
 import definition_pb2_grpc
 
 
-class Definition(definition_pb2_grpc.DefinitionsServicer):
+class Definitions(definition_pb2_grpc.DefinitionsServicer):
     def PingService(self, request, context):
         response = definition_pb2.PingResponse(ack="hola")
         return response
@@ -13,7 +14,13 @@ class Definition(definition_pb2_grpc.DefinitionsServicer):
 
 def main():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
-    definition_pb2_grpc.add_DefinitionsServicer_to_server(Definition(), server)
+    definition_pb2_grpc.add_DefinitionsServicer_to_server(Definitions(), server)
+
+    SERVICE_NAMES = (
+        definition_pb2.DESCRIPTOR.services_by_name['Definitions'].full_name,
+        reflection.SERVICE_NAME,
+    )
+    reflection.enable_server_reflection(SERVICE_NAMES, server)
 
     server.add_insecure_port('[::]:80')
     server.start()
